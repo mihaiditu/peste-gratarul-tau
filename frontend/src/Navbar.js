8083ccb0a5ddfc115011a0bd058afa16fc30da18
mihaiditu <mihaiditu2596@gmail.com>
2025-12-04 19:58:46 +0200

    adaugat logo pimp your grill IT

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -1,3 +1,5 @@
+import Group89 from './images/Group 89.svg';
+
 function Navbar() {
   return (
     <div
@@ -8,8 +10,15 @@ function Navbar() {
         borderRadius: '0 0 25px 25px',
         boxShadow: '0px 1px 14px #021B2C',
         position: 'relative',
+        display: 'flex',
+        alignItems: 'center',
       }}
-    />
+    >
+      <img
+        src={Group89}
+        style={{ height: '56px', marginLeft: '38px', display: 'block' }}
+      />
+    </div>
   );
 }
 
