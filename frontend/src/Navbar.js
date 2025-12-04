4bb1e195f714ab2f9ab36d5a17a29437da944347
mihaiditu <mihaiditu2596@gmail.com>
2025-12-04 19:39:28 +0200

    umbra la navbar

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -1,7 +1,15 @@
 function Navbar() {
   return (
-<div style={{ width: '100%', height: '97px', backgroundColor: '#721D08' }}>
-    </div>
+    <div
+      style={{
+        width: '100%',
+        height: '97px',
+        backgroundColor: '#721D08',
+        borderRadius: '0 0 25px 25px',
+        boxShadow: '0px 1px 14px #021B2C',
+        position: 'relative',
+      }}
+    />
   );
 }
 
