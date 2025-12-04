df73a2073d5dd00cc798ce95490eb1145955c3f3
mihaiditu <mihaiditu2596@gmail.com>
2025-12-04 18:56:40 +0200

    schimbat culoare navbar

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -1,6 +1,6 @@
 function Navbar() {
   return (
-    <div style={{ width: '100%', height: '60px', backgroundColor: 'red' }}>
+<div style={{ width: '100%', height: '60px', backgroundColor: '#721D08' }}>
     </div>
   );
 }
